776ff58e0b80ddbebf0aa2fd4da8f614d952757a
Sofie Karpowiczova <sofikarp@gmail.com>
2020-06-10 13:43:56 +0200

    Element IO added

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from "vue";
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 import App from "./App.vue";
 import router from "./router";
 
 Vue.config.productionTip = false;
+Vue.use(ElementUI, { size: 'small', zIndex: 3000 });
 
 new Vue({
   router,
