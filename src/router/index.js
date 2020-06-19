046d67bd250e7a39a9662d746c430d853aae1054
Daniela <dinving@gmail.com>
2020-06-19 19:56:14 +0200

    propojeni mandatory fields

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Create from "../views/Create.vue";
 import Guide from "../views/Guide.vue";
+import MandatoryFields from "../views/MandatoryFields.vue";
 import PropertyMetadata from "../views/PropertyMetadata.vue";
 
 Vue.use(VueRouter);
@@ -22,6 +23,12 @@ const routes = [
     path: "/region-merchant-provider/:id",
     name: "RegionMerchantProvider",
     component: PropertyMetadata,
+    props: true
+  },
+  {
+    path: "/mandatory-fields/:id",
+    name: "MandatoryFields",
+    component: MandatoryFields,
     props: true
   },
   // {
--- src/views/MandatoryFields.vue
+++ src/views/MandatoryFields.vue
@@ -1,91 +1,102 @@
 <template>
-  <div>
-    <h2>1. Company Information - part I</h2>
-    <el-main>Nějaký text, který vysvětlí, co a jak vyplnit.....koho se údaje týkají atd. A že jsou to povinná pole atd....</el-main>
-    <!-- Formulář--vše kromě polí s validací type number-->
+  <LayoutCard title="Region & Merchant Provider">
+    <Stepper :active="1" />
+    <div>
+      <h2>1. Company Information - part I</h2>
+      <el-main>Nějaký text, který vysvětlí, co a jak vyplnit.....koho se údaje týkají atd. A že jsou to povinná pole atd....</el-main>
+      <!-- Formulář--vše kromě polí s validací type number-->
 
-    <el-form
-      :model="ruleForm1"
-      :rules="rules"
-      ref="ruleForm1"
-      label-width="300px"
-      class="demo-ruleForm"
-      label="top"
-    >
-      <!-- Pole Official Company name. Validace na vyplněnost - nutná min. délka-->
-      <el-form-item label="Official Company name" prop="companyName">
-        <el-input v-model="ruleForm1.companyName"></el-input>
-      </el-form-item>
-      <!-- Pole Property Name. Validace na vyplněnost - nutná min. délka-->
-      <el-form-item label="Property Name" prop="propertyName">
-        <el-input v-model="ruleForm1.propertyName"></el-input>
-        <br />
-        <!-- Tooltip, reaguje na najetí myší-->
+      <el-form
+        :model="ruleForm1"
+        :rules="rules"
+        ref="ruleForm1"
+        label-width="300px"
+        class="demo-ruleForm"
+        label="top"
+      >
+        <!-- Pole Official Company name. Validace na vyplněnost - nutná min. délka-->
+        <el-form-item label="Official Company name" prop="companyName">
+          <el-input v-model="ruleForm1.companyName"></el-input>
+        </el-form-item>
+        <!-- Pole Property Name. Validace na vyplněnost - nutná min. délka-->
+        <el-form-item label="Property Name" prop="propertyName">
+          <el-input v-model="ruleForm1.propertyName"></el-input>
+          <br />
+          <!-- Tooltip, reaguje na najetí myší-->
 
-        <el-tooltip class="item" effect="light" content="Doing business as" placement="top-start">
-          <el-button>ℹ️ What is Property name?</el-button>
-        </el-tooltip>
-      </el-form-item>
-    </el-form>
-    <!-- Formulář - pro pole s validací number-->
-    <!-- Pole Company number. Validace na number-->
-    <el-form
-      :model="numberValidateForm"
-      ref="numberValidateForm"
-      :rules="rules"
-      label-width="300px"
-      class="demo-ruleForm"
-      label="top"
-    >
-      <el-form-item label="Company Registration Number" prop="registrationNumber">
-        <el-input v-model.number="numberValidateForm.registrationNumber" autocomplete="off"></el-input>
-      </el-form-item>
-    </el-form>
-    <!-- Formulář--vše kromě polí s validací type number-->
-    <el-form
-      :model="ruleForm"
-      :rules="rules"
-      ref="ruleForm"
-      label-width="300px"
-      class="demo-ruleForm"
-      label="top"
-    >
-      <!-- Pole Official Company Registred Address. Validace na vyplněnost - nutná min. délka-->
-      <el-form-item label="Company Registred Address" prop="registeredAddress">
-        <el-input v-model="ruleForm.registeredAddress"></el-input>
-      </el-form-item>
-      <!-- Pole Trading Address. Validace na vyplněnost - nutná min. délka-->
-      <el-form-item label="Trading Address" prop="tradingAddress">
-        <el-input v-model="ruleForm.tradingAddress"></el-input>
+          <el-tooltip class="item" effect="light" content="Doing business as" placement="top-start">
+            <el-button>ℹ️ What is Property name?</el-button>
+          </el-tooltip>
+        </el-form-item>
+      </el-form>
+      <!-- Formulář - pro pole s validací number-->
+      <!-- Pole Company number. Validace na number-->
+      <el-form
+        :model="numberValidateForm"
+        ref="numberValidateForm"
+        :rules="rules"
+        label-width="300px"
+        class="demo-ruleForm"
+        label="top"
+      >
+        <el-form-item label="Company Registration Number" prop="registrationNumber">
+          <el-input v-model.number="numberValidateForm.registrationNumber" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <!-- Formulář--vše kromě polí s validací type number-->
+      <el-form
+        :model="ruleForm"
+        :rules="rules"
+        ref="ruleForm"
+        label-width="300px"
+        class="demo-ruleForm"
+        label="top"
+      >
+        <!-- Pole Official Company Registred Address. Validace na vyplněnost - nutná min. délka-->
+        <el-form-item label="Company Registred Address" prop="registeredAddress">
+          <el-input v-model="ruleForm.registeredAddress"></el-input>
+        </el-form-item>
+        <!-- Pole Trading Address. Validace na vyplněnost - nutná min. délka-->
+        <el-form-item label="Trading Address" prop="tradingAddress">
+          <el-input v-model="ruleForm.tradingAddress"></el-input>
+          <br />
+          <!-- Popover, reaguje na najetí myší-->
+          <el-popover
+            placement="top-start"
+            type="warning"
+            width="200"
+            trigger="hover"
+            content="`This is the location from where the company conducts its business; it can also be the address of the hotel/property.`"
+            class="popup"
+          >
+            <el-button slot="reference">ℹ️ What is Trading address</el-button>
+          </el-popover>
+        </el-form-item>
+        <!-- validace website -->
+        <el-form-item label="Website" prop="website">
+          <el-input v-model="ruleForm.website"></el-input>
+        </el-form-item>
         <br />
-        <!-- Popover, reaguje na najetí myší-->
-        <el-popover
-          placement="top-start"
-          type="warning"
-          width="200"
-          trigger="hover"
-          content="`This is the location from where the company conducts its business; it can also be the address of the hotel/property.`"
-          class="popup"
-        >
-          <el-button slot="reference">ℹ️ What is Trading address</el-button>
-        </el-popover>
-      </el-form-item>
-      <!-- validace website -->
-      <el-form-item label="Website" prop="website">
-        <el-input v-model="ruleForm.website"></el-input>
-      </el-form-item>
-      <br />
-      <!-- button -->
-      <el-form-item>
-        <el-button type="primary" @click="submitForm('ruleForm')">Create</el-button>
-        <el-button @click="resetForm('ruleForm')">Reset</el-button>
-      </el-form-item>
-    </el-form>
-  </div>
+        <!-- button -->
+        <el-form-item>
+          <el-button type="primary" @click="submitForm('ruleForm')">Create</el-button>
+          <el-button @click="resetForm('ruleForm')">Reset</el-button>
+        </el-form-item>
+        <BackNext :show-back="false" v-on:next="onNext" />
+      </el-form>
+    </div>
+  </LayoutCard>
 </template>
 
 <script>
+import LayoutCard from "@/components/LayoutCard";
+import Stepper from "@/components/Stepper";
+import BackNext from "@/components/BackNext";
+
 export default {
+  name: "MandatoryFields",
+  components: { LayoutCard, Stepper, BackNext },
+  props: ["id", "form"],
   data() {
     return {
       ruleForm1: {
@@ -177,41 +188,39 @@ export default {
     };
   },
   methods: {
-    submitForm(formName) {
-      this.$refs["ruleForm1"].validate(valid => {
-        if (valid) {
-          alert("submit!");
-        } else {
-          console.log("error submit!!");
-          return false;
-        }
-      });
-
-      this.$refs["numberValidateForm"].validate(valid => {
-        if (valid) {
-          alert("Please submit!");
-        } else {
-          console.log("error submit!!");
-          return false;
-        }
-      });
-
-      this.$refs["ruleForm"].validate(valid => {
-        if (valid) {
-          alert("Please submit!");
-        } else {
-          console.log("error submit!!");
-          return false;
-        }
-      });
-    },
-    resetForm(formName) {
-      this.$refs["ruleForm1"].resetFields();
-      this.$refs["numberValidateForm"].resetFields();
-      this.$refs["ruleForm"].resetFields();
-    }
+    //submitForm(formName) {
+    //   this.$refs["ruleForm1"].validate(valid => {
+    //     if (valid) {
+    //       alert("submit!");
+    //     } else {
+    //       console.log("error submit!!");
+    //      return false;
+    //     }
+    //  });
+    //   this.$refs["numberValidateForm"].validate(valid => {
+    //     if (valid) {
+    //       alert("Please submit!");
+    //     } else {
+    //       console.log("error submit!!");
+    //       return false;
+    //     }
+    //   });
+    //   this.$refs["ruleForm"].validate(valid => {
+    //    if (valid) {
+    //      alert("Please submit!");
+    //    } else {
+    //      console.log("error submit!!");
+    //      return false;
+    //    }
+    //  });
   }
+  //resetForm(formName) {
+  // this.$refs["ruleForm1"].resetFields();
+  //  this.$refs["numberValidateForm"].resetFields();
+  //  this.$refs["ruleForm"].resetFields();
+  // }
 };
+//};
 </script>
 
 <style>
