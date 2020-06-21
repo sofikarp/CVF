d29fc637da158a537106cbe0c3363d1e240e611e
Sofie Karpowiczova <sofikarp@gmail.com>
2020-06-21 18:16:59 +0200

    backend const

--- src/services/axios.js
+++ src/services/axios.js
@@ -3,17 +3,19 @@ import axios from "axios"
 axios.defaults.headers.common["Content-Type"] =
   "application/json; charset=utf-8";
 
+const backend = "https://cvf.jiprochazka.com"
+
 // Požedavek post na server - posílání dat na server - používá se v tomto případě Create, požívá se při zakládání
 export const post = (url, data) => {
-  return axios.post(`${process.env.VUE_APP_API_URL}${url}`, data)
+  return axios.post(`${backend}${url}`, data)
 }
 
 // Požadavek get na server - vrací data ze serveru - používá v tomto případě v Appu při načítání dat, načtou se data a předají se do view komponent
 export const get = (url) => {
-  return axios.get(`${process.env.VUE_APP_API_URL}${url}`)
+  return axios.get(`${backend}${url}`)
 }
 
 // Požadavek put na server - posílá data na server - používá v tomto případě v Appu při odesílání dat, používá se při updatu
 export const put = (url, data) => {
-  return axios.put(`${process.env.VUE_APP_API_URL}${url}`, data)
+  return axios.put(`${backend}${url}`, data)
 }
