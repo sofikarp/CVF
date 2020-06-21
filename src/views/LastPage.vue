cb249c107d7feaa33469de615bffdf50602fe65f
Daniela <dinving@gmail.com>
2020-06-21 17:42:15 +0200

    styly a gif

--- src/views/LastPage.vue
+++ src/views/LastPage.vue
@@ -2,11 +2,13 @@
   <LayoutCard title>
     <div>
       <h1>It's done!</h1>
-      <br />Thank you for filling the Customer Verification Form, we will come
-      back to you soon.
       <br />
-
-      <i class="el-icon-success"></i>
+      <div class="text">
+        Thank you for filling the Customer Verification Form, we will come back
+        to you soon.
+      </div>
+      <br />
+      <img src="./img/done.gif" />
     </div>
   </LayoutCard>
 </template>
@@ -27,5 +29,22 @@ i {
 
 div {
   text-align: center;
+}
+img {
+  padding: 30px;
+  text-align: center;
+}
+.text {
+  color: #303133;
+  padding: 0 20px 20px 0;
+  font-size: 14px;
+  line-height: 1.5;
+}
+h1 {
+  font-size: 20px large;
+  text-align: center;
+  font-weight: bold;
+  color: #409eff;
+  padding-bottom: 20px;
 }
 </style>
